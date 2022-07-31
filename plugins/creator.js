e9cfec38ceca6d44940bcf9c769997a6d7dda30b
alpinnio <107537249+alpinnio@users.noreply.github.com>
2022-07-31 11:43:58 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 
-  let i = global.owner.map(([number]) => number).map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net')
+  let i = global.owner.map(([number]) => number).map(v => v.replace(/[^0-9]/g, '') + '6288210072756@s.whatsapp.net')
   
   
   this.sendContact(m.chat, data.map(([id, name]) => [id, this.getName(i[0])]), m)
